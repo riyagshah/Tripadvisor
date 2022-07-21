ca223ee2a0ac2f4fdc4d97abb4386a439e8819e2
riyagshah <riyagshah4@gmail.com>
2022-07-22 01:29:58 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Homepage from './Components/Homepage';
+import Homepage from './Components/Homepage/Homepage';
 function App() {
   return (
     <div className="App">
