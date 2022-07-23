3fce1d44199e88960197840a02b7e9bc48bb7dd9
Shubh-Pawar007 <shubhampawar242767@gmail.com>
2022-07-23 20:36:45 +0530

    day5 update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Homepage from './Components/Homepage/Homepage';
+import Page_one from './Pages/Things_to_Do/Page_one';
 
 function App() {
   return (
     <div className="App">
-<Homepage/>
+      {/* <Homepage /> */}
+      <Page_one/>
 
 
     </div>
